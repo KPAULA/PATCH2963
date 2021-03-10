55c1eabe650d02ea73718a675cef3f1d40e570d3
Wendel Albino <wendelalbino@compliancefiscal.com.br>
2021-03-10 17:44:38 -0300

    #75976
    criacao trigger de cte e alteracao de triger de nf para nao deixar alterar status de nfs denegada, cancelada ou inutilizada

--- Triggers/catalogar.sql
+++ Triggers/catalogar.sql
@@ -1,11 +1,19 @@
 Prompt Catalogar Triggers no CSF_OWN
 
-prompt T_A_U_ITEM_AGEND_INTEGR_01.trg
-@@T_A_U_ITEM_AGEND_INTEGR_01.trg
+prompt T_B_I_U_Nota_Fiscal_02
+@@T_B_I_U_Nota_Fiscal_02.trg
 show err
 
-prompt t_b_u_nota_fiscal_02.trg
-@@t_b_u_nota_fiscal_02.trg
+Prompt T_B_U_Nota_Fiscal_01
+@@T_B_U_Nota_Fiscal_01.trg
+show err
+
+Prompt T_B_U_Conhec_transp_01.trg
+@@T_B_U_Conhec_transp_01.trg
+show err
+
+Prompt T_A_U_ITEM_AGEND_INTEGR_01.trg
+@@T_A_U_ITEM_AGEND_INTEGR_01.trg
 show err
 
 Prompt Catalogar Objetos Invalidos.trg
