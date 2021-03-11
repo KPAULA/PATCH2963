c99554687d6371f8719bd3ced2e1fd69fb7de488
Wendel Albino <wendelalbino@compliancefiscal.com.br>
2021-03-11 12:59:27 -0300

    #76700
      -- Redmine #76700 - Falha no agendamento de integracao - status agendado (SPANI)
      -- Redmine #76654 - Falha no agendamento de integracao - status agendado (ACECO)
      --                - criada validacao que se a data do agendamento for <= sysdate, adiciona 15 segundos na data.

--- Triggers/catalogar.sql
+++ Triggers/catalogar.sql
@@ -12,6 +12,10 @@ Prompt T_B_U_Conhec_transp_01.trg
 @@T_B_U_Conhec_transp_01.trg
 show err
 
+prompt T_A_U_ITEM_AGEND_INTEGR_01.trg
+@@T_A_U_ITEM_AGEND_INTEGR_01.trg
+show err
+
 Prompt Catalogar Objetos Invalidos.trg
 @@rec_obj_invalid.sql.
 
