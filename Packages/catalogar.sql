9b5fe6d268106937d0e901d642aa5135129a3903
KPAULA <karina.paula@compliancefiscal.com.br>
2021-03-26 09:53:42 -0300

    Update catalogar.sql
    Redmine 75869	- Falha no retorno de cancelamento

--- Packages/catalogar.sql
+++ Packages/catalogar.sql
@@ -11,11 +11,39 @@ prompt 31b_CSF_OWN_pk_csf_api
 @@31b_CSF_OWN_pk_csf_api.pkb
 show err
 
+prompt 33a_CSF_OWN_pk_valida_ambiente
+@@33a_CSF_OWN_pk_valida_ambiente.pks
+show err
+prompt 33b_CSF_OWN_pk_valida_ambiente
+@@33b_CSF_OWN_pk_valida_ambiente.pkb
+show err
+
 prompt 41a_CSF_OWN_pk_valida_ambiente_ct.pks
 @@41a_CSF_OWN_pk_valida_ambiente_ct.pks
 show err
 prompt 41b_CSF_OWN_pk_valida_ambiente_ct.pkb
 @@41b_CSF_OWN_pk_valida_ambiente_ct.pkb
+show err
+
+prompt 58a_pk_integr_bloco_ecd
+@@58a_pk_integr_bloco_ecd.pks
+show err
+prompt 58b_pk_integr_bloco_ecd
+@@58b_pk_integr_bloco_ecd.pkb
+show err
+
+prompt 60a_pk_csf_api_sc
+@@60a_pk_csf_api_sc.pks
+show err
+prompt 60b_pk_csf_api_sc
+@@60b_pk_csf_api_sc.pkb
+show err
+
+prompt 67a_pk_int_view_sc
+@@67a_pk_int_view_sc.pks
+show err
+prompt 67b_pk_int_view_sc
+@@67b_pk_int_view_sc.pkb
 show err
 
 prompt 71a_pk_csf_efd
@@ -32,6 +60,61 @@ prompt 171b_pk_vld_amb_sc.pkb
 @@171b_pk_vld_amb_sc.pkb
 show err
 
+prompt 200a_pk_int_nfe_sap
+@@200a_pk_int_nfe_sap.pks
 show err
+prompt 200b_pk_int_nfe_sap
+@@200b_pk_int_nfe_sap.pkb
+show err
+
+prompt 211a_pk_despr_integr.pks
+@@211a_pk_despr_integr.pks
+show err
+prompt 211b_pk_despr_integr.pkb
+@@211b_pk_despr_integr.pkb
+show err
+
+prompt 221a_csf_own_pk_csf_api_nfs
+@@221a_csf_own_pk_csf_api_nfs.pks
+show err
+prompt 221b_csf_own_pk_csf_api_nfs
+@@221b_csf_own_pk_csf_api_nfs.pkb
+show err
+
+prompt 360a_pk_entr_nfe_terceiro
+@@360a_pk_entr_nfe_terceiro.pks
+show err
+prompt 360b_pk_entr_nfe_terceiro
+@@360b_pk_entr_nfe_terceiro.pkb
+show err
+
+prompt 420a_pk_subapur_icms
+@@420a_pk_subapur_icms.pks
+show err
+prompt 420b_pk_subapur_icms
+@@420b_pk_subapur_icms.pkb
+show err
+
+prompt 463a_pk_gera_arq_secf 
+@@463a_pk_gera_arq_secf.pks 
+show err
+prompt 463b_pk_gera_arq_secf 
+@@463b_pk_gera_arq_secf.pkb
+show err
+
+prompt 547a_pk_int_importnfse.pks
+@@547a_pk_int_importnfse.pks
+show err
+prompt 547b_pk_int_importnfse
+@@547b_pk_int_importnfse.pkb
+show err
+
+prompt 651a_CSF_OWN_pk_csf_api_nfce
+@@651a_CSF_OWN_pk_csf_api_nfce.pks
+show err
+prompt 651b_CSF_OWN_pk_csf_api_nfce
+@@651b_CSF_OWN_pk_csf_api_nfce.pkb
+show err
+
 Prompt Catalogar Objetos Invalidos
 @@rec_obj_invalid.sql
