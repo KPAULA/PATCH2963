76b10b54e4b1bd95c4009f5a497336a872283fce
KPAULA <karina.paula@compliancefiscal.com.br>
2021-03-26 09:43:11 -0300

    Update catalogar.sql
    Redmine 77107 - Pedido de cancelamento rejeitado - situação do documento (FRANKLIN)

--- Triggers/catalogar.sql
+++ Triggers/catalogar.sql
@@ -8,6 +8,11 @@ prompt t_b_u_nota_fiscal_02.trg
 @@t_b_u_nota_fiscal_02.trg
 show err
 
+prompt T_B_I_U_Nota_Fiscal_99.trg
+@@T_B_I_U_Nota_Fiscal_99.trg
+show err
+
+
 Prompt Catalogar Objetos Invalidos.trg
 @@rec_obj_invalid.sql.
 
